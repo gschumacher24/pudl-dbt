5b2c875a269cb559b00ed2ea4d58ad32b811adc4
gschumacher24 <116086712+gschumacher24@users.noreply.github.com>
2025-09-15 15:02:12 -0500

    feat: added estimated demand (#14)
    updated generate_database_name.sql to factor in individual user environments

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,11 +1,12 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
 
     {%- if  target.name == "dev_pr" -%} s_dev_pudl
-    {%- elif target.name == "dev"  -%} s_dev_pudl
-    {%- elif target.name == "qa_pr"  -%} s_qa_pudl
-    {%- elif target.name == "qa"  -%} s_qa_pudl
-    {%- elif target.name == "prod_pr"  -%} s_prod_pudl
-    {%- elif target.name == "prod"  -%} s_prod_pudl
+    {%- elif target.name == "dev" -%} s_dev_pudl
+    {%- elif target.name == "qa_pr" -%} s_qa_pudl
+    {%- elif target.name == "qa" -%} s_qa_pudl
+    {%- elif target.name == "prod_pr" -%} s_prod_pudl
+    {%- elif target.name == "prod" -%} s_prod_pudl
+    {%- elif "user_" in target.name -%} {{ target.name }}
     {%- else -%} invalid_database
     {%- endif -%}
 
