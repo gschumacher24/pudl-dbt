ab50814806a65e0fe8a73ff1a74c7863d4b6f69d
gschumacher <gschumacher@phdata.io>
2025-09-15 13:55:32 -0500

    updated generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,13 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
+    {%- if default_schema == 'dbt_cloud_pr' -%}
+
+        {{ default_schema }}_{{ custom_schema_name}}
+
     {%- if custom_schema_name is none -%}
 
         {{ default_schema }}
-
-    {%- elif default_schema == 'dbt_cloud_pr' -%}
-
-        {{ default_schema }}_{{ custom_schema_name}}
 
     {%- else -%}
 
